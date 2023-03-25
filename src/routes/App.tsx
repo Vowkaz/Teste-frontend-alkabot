f082f4e255fd5094f9836ecb3e97f955efd58925
vowkaz <jlucasgarmier@gmail.com>
2023-03-25 17:15:38 -0400

    fix path / redirect to post

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,14 +1,24 @@
 import {Navbar} from "../components/NavBar";
 import {Outlet} from "react-router-dom";
+import axios from "axios";
+import {useEffect} from "react";
 
 export const App = () => {
+    const ref = window.location.href.slice(-1)
+    useEffect(()=>{
+        if(ref=== '/') {
+            axios({}).catch(err => window.location.href = "/posts")
+        }
+    },[])
+
     return (
         <>
             <main
-            className={`min-h-screen h-max w-auto bg-gradient-to-bl from-gray-200 to-gray-100`}>
-                <Navbar />
+                className={`min-h-screen h-max w-auto bg-gradient-to-bl from-gray-200 to-gray-100`}>
+                <Navbar/>
 
-                <Outlet />
+                <Outlet/>
+
             </main>
         </>
     )
