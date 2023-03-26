a86320fedfc8555b48314182ab1a431d616ec744
vowkaz <jlucasgarmier@gmail.com>
2023-03-26 19:51:37 -0400

    requisicao sem uso removida

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,13 +1,12 @@
 import {Navbar} from "../components/NavBar";
 import {Outlet} from "react-router-dom";
 import {useEffect} from "react";
-import {api} from "../../lib/axios";
 
 export const App = () => {
     const ref = window.location.href.slice(-1)
     useEffect(()=>{
         if(ref !== 's') {
-            api.get('user/1').catch(err => window.location.href = "/posts").then((resp)=>window.location.href = "/posts")
+            window.location.href = "/posts"
         }
     },[])
 
